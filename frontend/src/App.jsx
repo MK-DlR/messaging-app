8237c6a0a77da77666974c75a38f6d191f4c791c
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-02-26 15:29:00 -0500

    feat: install react router in frontend
    set up BrowserRouter, Routes, and individual Route in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 // frontend/src/App.jsx
 
+// imports
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 function App() {
   return (
     <>
-      <div>
-        hi
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<div>Register</div>} />
+          <Route path="/login" element={<div>Login</div>} />
+          <Route path="/" element={<div>Home</div>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 // frontend/src/main.jsx
 
+// imports
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
