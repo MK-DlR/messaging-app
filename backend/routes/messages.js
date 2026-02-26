fd471cfb663e6b9bdb17b28dfcab5266804bf347
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-02-26 12:33:54 -0500

    feat: set up messages router

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -6,12 +6,16 @@ const messagesController = require("../controllers/messages.js");
 const { authJWT } = require("../middleware/auth.js");
 const { lastSeen } = require("../middleware/lastSeen.js");
 
-// send messages
+// create a message
+// router.post("/new-message", authJWT, lastSeen, messagesController.messagePost);
 
 // fetch messages within channel
+// router.get("/all-messages/:channelId", authJWT, lastSeen, messagesController.messagesGet);
 
 // editing (own) messages
+// router.put("/edit/:id", authJWT, lastSeen, messagesController.messagePut);
 
 // deleting (own) messages
+// router.delete("/delete/:id", authJWT, lastSeen, messagesController.messageDelete);
 
 module.exports = router;
