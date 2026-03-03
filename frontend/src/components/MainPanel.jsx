b9e42fdec0b74a817f3c6404216d2cb328f5e4d5
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-03-03 15:12:53 -0500

    refactor: change if/else mainPanel statement to switch statement

--- frontend/src/components/MainPanel.jsx
+++ frontend/src/components/MainPanel.jsx
@@ -49,77 +49,81 @@ function MainPanel( { currentUser, setCurrentUser, selectedChannel, setSelectedC
     let content;
 
     // determine if a channel is selected
-    if (mainPanelView === "messages") {       
-        // display channel name and details button
-        title =
-            <div className="channel-header header">
-                <h2>{selectedChannel.name}</h2>
-
-                <i 
-                    className="fa-solid fa-circle-info details-icon"
-                    onClick={() => {setMainPanelView("channelDetails")}}
-                />
-            </div>
-
-        // map over and display messages
-        content = messages.map(message =>
-            <div
-                key={message.id}
-                className="message"
-                // TO DO: if message author
-                // hovering shows edit and delete button
-            >
-                <div
-                    // clicking on user's name and/or icon opens user's profile
-                    onClick={() => {
-                        clearTimeout(clickTimer.current);
-                        clickTimer.current = setTimeout(() => {
-                            setSelectedUser(message.users);
-                            setMainPanelView("userProfile");
-                            console.log(message.users.username);
-                        }, 250)
-                    }}
-                    // double clicking on user's name and/or icon creates DM
-                    onDoubleClick={() => {
-                        clearTimeout(clickTimer.current);
-                        setMainPanelView("createChannel");
-                        console.log("TO DO: create new dm with user");}
-                    }
-                >
-                    <img className="message-icon icon" src={`/icons/${message.users.icon}`}></img> 
-                    {message.users.displayName || message.users.username} 
+    switch (mainPanelView) {
+        case "messages":
+            // display channel name and details button
+            title =
+                <div className="channel-header header">
+                    <h2>{selectedChannel.name}</h2>
+                    <i 
+                        className="fa-solid fa-circle-info details-icon"
+                        onClick={() => {setMainPanelView("channelDetails")}}
+                    />
                 </div>
-                {formatDate(message.createdAt)}<br />
-                {message.body}
-            </div>
-        )
-    } else if (mainPanelView === "channelDetails") {
-        // thing
-    } else if (mainPanelView === "userProfile") {
-        if (!userProfile) {
-            content = <div>Loading...</div>
-        } else {
-            content = 
-            <div className="user-profile">
-                <img className="profile-icon icon" src={`/icons/${userProfile.icon}`}></img>
-                {userProfile.displayName}
-                {userProfile.username}
-                {userProfile.profileInfo}
-                Last seen: {formatDate(userProfile.lastSeen)}
-                <i 
-                    className="fa-regular fa-envelope message-icon"
-                    onClick={() => {setMainPanelView("createChannel")}}
-                />
-                <i 
-                    className="fa-solid fa-x exit-icon" 
-                    onClick={() => setMainPanelView("messages")}
-                />
-            </div>
-        }
-    } else if (mainPanelView === "createChannel") {
-        content = <div>New DM...</div>
-    } else {
-        content = <div>Select a channel...</div>;
+    
+            // map over and display messages
+            content = messages.map(message =>
+                <div
+                    key={message.id}
+                    className="message"
+                    // TO DO: if message author
+                    // hovering shows edit and delete button
+                >
+                    <div
+                        // clicking on user's name and/or icon opens user's profile
+                        onClick={() => {
+                            clearTimeout(clickTimer.current);
+                            clickTimer.current = setTimeout(() => {
+                                setSelectedUser(message.users);
+                                setMainPanelView("userProfile");
+                                console.log(message.users.username);
+                            }, 250)
+                        }}
+                        // double clicking on user's name and/or icon creates DM
+                        onDoubleClick={() => {
+                            clearTimeout(clickTimer.current);
+                            setMainPanelView("createChannel");
+                            console.log("TO DO: create new dm with user");
+                        }}
+                    >
+                        <img className="message-icon icon" src={`/icons/${message.users.icon}`}></img> 
+                        {message.users.displayName || message.users.username} 
+                    </div>
+                    {formatDate(message.createdAt)}<br />
+                    {message.body}
+                </div>
+            )
+            break;
+        case "channelDetails":
+            // thing
+            break;
+        case "userProfile":
+            if (!userProfile) {
+                content = <div>Loading...</div>
+            } else {
+                content = 
+                <div className="user-profile">
+                    <img className="profile-icon icon" src={`/icons/${userProfile.icon}`}></img>
+                    {userProfile.displayName}
+                    {userProfile.username}
+                    {userProfile.profileInfo}
+                    Last seen: {formatDate(userProfile.lastSeen)}
+                    <i 
+                        className="fa-regular fa-envelope message-icon"
+                        onClick={() => {setMainPanelView("createChannel")}}
+                    />
+                    <i 
+                        className="fa-solid fa-x exit-icon" 
+                        onClick={() => setMainPanelView("messages")}
+                    />
+                </div>
+            }
+            break;
+        case "createChannel":
+            content = <div>New DM...</div>
+            break;
+        default:
+            content = <div>Select a channel...</div>;
     }
     
     return (
