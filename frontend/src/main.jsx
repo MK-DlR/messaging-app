f651325d69743508d06b8c82d3a900e538671e67
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-03-23 12:22:43 -0400

    refactor: split index.css into specific css files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,21 @@
 // imports
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import "./styles/theme.css";
+import "./styles/base.css";
+import "./styles/layout.css";
+import "./styles/header.css";
+import "./styles/channels.css";
+import "./styles/users.css";
+import "./styles/profile.css";
+import "./styles/messages.css";
+import "./styles/textbox.css";
+import "./styles/forms.css";
+import "./styles/auth.css";
+import "./styles/icons.css";
+import "./styles/buttons.css";
+import "./styles/misc.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
