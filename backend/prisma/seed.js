b6384e2eed7f19915087fe6b05a33e559f9d0230
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-03-25 15:16:15 -0400

    feat: create default guest account
    update seed script to create default guest account

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -1,5 +1,9 @@
 // backend/prisma/seed.js
 
+// imports
+const bcrypt = require("bcryptjs");
+const crypto = require("crypto");
+
 require("dotenv").config({
   path: require("path").resolve(__dirname, "../.env"),
 });
@@ -12,14 +16,14 @@ const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
-// create default channel
-async function main() {
-  const defaultChannel = await prisma.channel.findFirst({
+async function seed() {
+  // create default channel first
+  let defaultChannel = await prisma.channel.findFirst({
     where: { name: "Main Chat" },
   });
 
   if (!defaultChannel) {
-    const createChannel = await prisma.channel.create({
+    defaultChannel = await prisma.channel.create({
       data: {
         name: "Main Chat",
         isDefault: true,
@@ -27,9 +31,34 @@ async function main() {
       },
     });
   }
+
+  // generate and hash random password
+  const randomPassword = crypto.randomBytes(32).toString("hex");
+  const hashedPassword = await bcrypt.hash(randomPassword, 10);
+
+  // then create guest user and add to Main Chat
+  const guestUser = await prisma.user.findFirst({
+    where: { usernameNormalized: "guest" },
+  });
+
+  if (!guestUser) {
+    await prisma.user.create({
+      data: {
+        username: "Guest",
+        usernameNormalized: "guest",
+        password: hashedPassword,
+        displayName: "Guest Account",
+        profileInfo:
+          "This is a demo account for guest use, which cannot be edited. Please note that anyone is able to access this demo account.",
+        channels: {
+          connect: { id: defaultChannel.id },
+        },
+      },
+    });
+  }
 }
 
-main()
+seed()
   .then(async () => {
     await prisma.$disconnect();
   })
