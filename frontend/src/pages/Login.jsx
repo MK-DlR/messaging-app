90ce08847073fdf7f6e39ed59af2b2d0e17af7f6
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-03-25 15:32:45 -0400

    feat: implement guest login button functionality

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -49,6 +49,41 @@ function Login() {
         }
     }
 
+    async function handleGuestLogin() {
+        // block submission if API isn't ready
+        if (isCheckingApi) {
+            setError("Backend is still loading. Please wait...");
+            return;
+        }
+
+        const confirmed = window.confirm(
+            "This is a demo account for guest use. Some functionality may not be available. " +
+            "Please note that anyone is able to access this demo account.\n\n" +
+            "Continue as guest?"
+        );
+        
+        if (!confirmed) return;
+
+        const response = await fetch(
+            `${import.meta.env.VITE_API_URL}/users/guest-login`, 
+            { 
+                method: "POST", 
+                headers: {"Content-Type": "application/json" }, 
+            }
+        )
+        const data = await response.json();
+
+        // check response
+        if (!response.ok) {
+            setError(data.error);
+        } else {
+            // if successful login, store JWT token
+            localStorage.setItem("token", data.token)
+            // redirect to home
+            navigate("/");
+        }
+    }
+
     return (
         <div 
             className="login-page"
@@ -95,7 +130,14 @@ function Login() {
                     </label>
                     <button className="submit button" type="submit">Login</button>
                 </form>
-                <button className="guest button" type="submit">Login As Guest</button>
+                <button 
+                    className="guest button"
+                    onClick={handleGuestLogin}
+                    style={{ opacity: !isApiReady ? 0.5 : 1 }}
+                    disabled={!isApiReady}
+                >
+                    Login As Guest
+                </button>
                 <div className="register-here">Don't have an account? Register <a href="/register">here</a>.</div>
             </div>
             <div className="footer"><a href="https://github.com/MK-DlR">Created by <i class="fa-brands fa-github" /> MK-DlR</a></div>
